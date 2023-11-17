8af3553ee992b8c10452a121ca7a1687a7d5d53f
MArko Gilic <gilicmarko10@gmail.com>
2023-11-17 21:22:23 +0100

    new update

--- app/components/EmployeeCard.jsx
+++ app/components/EmployeeCard.jsx
@@ -1,19 +1,31 @@
 import Link from 'next/link';
 
 import ProfileImage from './ProfileImage';
+import ShowRole from './ShowRole';
 
 const Employee = ({ employee }) => {
   const { name, status, role } = employee;
 
   const srcImage = 'https://100k-faces.glitch.me/random-image';
+
   return (
-    <div className="employeeCard">
+    <div className="employeeCard relative">
+      <div className="absolute top-3 right-5">
+        {status === 'active' && (
+          <span className="text-green-500"> Active ●</span>
+        )}
+        {status === 'inactive' && (
+          <span className="text-red-500"> Inactive ●</span>
+        )}
+      </div>
       <div className="flex flex-col items-center py-10">
         {/* <img src={srcImage} alt="some image" className="profileImg" /> */}
         <ProfileImage src={srcImage} layouClass="profileImg" />
 
         <h5 className="mb-1 text-xl font-medium text-gray-900">{name}</h5>
-        <span className="text-sm text-gray-500">{role}</span>
+        <span className="text-lg text-gray-500">
+          <ShowRole id={role} />
+        </span>
         <div className="flex mt-4 justify-center  ">
           <Link href="/employees" className="emplyeeBtn">
             Go Back
